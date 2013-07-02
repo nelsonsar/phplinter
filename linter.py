5ac2c7d5cd87f53ae2e27e85ec87833ce053c96e
Nelson Senna <onelsonsenna@gmail.com>
2013-07-02 18:05:52 -0300

    Implementing verbose option
    * Add help, verbose and quiet options to linter;
    * Replace subprocess.call for subprocess.Popen objects
      to capture stderr and stdout;

--- linter.py
+++ linter.py
@@ -1,10 +1,24 @@
 #! /usr/bin/env python
 
 from phplint import PHPLint
-import sys
+from optparse import OptionParser
 
 def main():
-    linter = PHPLint()
-    linter.lint(sys.argv[1])
+    usage = "%prog [options] path";
+    parser = OptionParser(usage=usage)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+                      help="This is default option. It will show result for each file",
+                      default=True)
+    parser.add_option("-q", "--quiet", dest="verbose", action="store_false",
+                      help="Show only stderr output")
 
-main()
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("You must specify a path to lint")
+
+    linter = PHPLint()
+    linter.silent = not options.verbose
+    linter.lint(args[0])
+
+if __name__ == "__main__":
+    main()
