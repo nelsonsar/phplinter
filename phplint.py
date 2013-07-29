ba26ebeaf33b321b57f2750de9cabe393711660c
Nelson Senna <onelsonsenna@gmail.com>
2013-07-28 21:11:12 -0300

    Change code to be compliant with PEP 8
    This change camel cased functions for underscored function names and
    properties.

--- phplint.py
+++ phplint.py
@@ -5,33 +5,34 @@ class PHPLint:
     def __init__(self):
         self.silent = False
 
-    def setSilentLint(self, isSilent):
-        self.silent = isSilent
+    def set_silent_lint(self, is_silent):
+        self.silent = is_silent
 
     def lint(self, path):
         if os.path.isfile(path):
-            self.lintFile(path)
+            self.lint_file(path)
         elif os.path.isdir(path):
-            self.lintDir(path)
+            self.lint_dir(path)
 
-    def lintFile(self, path):
-        if self.isPHPFile(path):
-            process = subprocess.Popen(['php', '-l', path], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            (processStdOut, processStdErr) = process.communicate();
-            if not self.isSilentLint():
-                print processStdOut.rstrip()
+    def lint_file(self, path):
+        if self.is_php_file(path):
+            process = subprocess.Popen(['php', '-l', path], shell=False,
+                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            (process_stdout, process_stderr) = process.communicate();
+            if not self.is_silent_lint():
+                print process_stdout.rstrip()
             if process.returncode > 0:
-                print processStdErr.rstrip()
+                print process_stderr.rstrip()
                 raise SystemExit(1)
 
-    def lintDir(self, path):
-        for rootDir, dirName, files in os.walk(path):
+    def lint_dir(self, path):
+        for root_dir, dir_name, files in os.walk(path):
             for f in files:
-                self.lintFile(os.path.join(rootDir, f))
+                self.lint_file(os.path.join(root_dir, f))
 
-    def isPHPFile(self, filename):
+    def is_php_file(self, filename):
         return filename.endswith('.php')
 
-    def isSilentLint(self):
+    def is_silent_lint(self):
         return self.silent
 
